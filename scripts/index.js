ccd22abf6e14e8ed0aa8e3f4c8c61dbddf1d8970
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2022-06-22 22:48:02 +0300

    Реализация логики добавления карточки и редактирования профиля

--- scripts/index.js
+++ scripts/index.js
@@ -11,40 +11,74 @@ const addPhotoButton = page.querySelector(".profile__add-button");
 const popupProfile = page.querySelector(".popup_profile");
 const popupPhoto = page.querySelector(".popup_photo");
 
-// Инпуты и кнопки попапа для профиля
+// Инпуты, форма и кнопки попапа для профиля
+const profileForm = popupProfile.querySelector(".form_profile");
 const popupProfileName = popupProfile.querySelector(".form__input_field_username");
 const popupProfileJob = popupProfile.querySelector(".form__input_field_job");
-const popupProfileSubmitBtn = popupProfile.querySelector(".form__submit-button_type_profile");
 const popupProfileCloseBtn = popupProfile.querySelector(".popup__close-button_type_profile");
 
-// Инпуты и кнопки попапа для фотокарточек
+// Инпуты, форма и кнопки попапа для фотокарточек
+const photoForm = popupPhoto.querySelector(".form_photo");
 const popupPhotoTitle = popupPhoto.querySelector(".form__input_field_title");
 const popupPhotoLink = popupPhoto.querySelector(".form__input_field_photo");
-const popupPhotoSubmitBtn = popupPhoto.querySelector(".form__submit-button_type_photo");
 const popupPhotoCloseBtn = popupPhoto.querySelector(".popup__close-button_type_photo");
 
 //Функция открытия popup
 function popupOpen(newPopup) {
   const closeBtn = newPopup.querySelector(".popup__close-button");
-  closeBtn.addEventListener('click', function (event) {
+  closeBtn.addEventListener("click", function (event) {
     event.preventDefault();
     newPopup.classList.toggle("popup_is_opened");
   });
   return newPopup;
 }
 
-//Слушатели событий
-editProfileButton.addEventListener('click', function(event) {
+//Слушатели для открытия попапов
+editProfileButton.addEventListener("click", function (event) {
   event.preventDefault();
   popupProfile.classList.toggle("popup_is_opened");
-})
+});
 popupOpen(popupProfile);
 
-addPhotoButton.addEventListener('click', function(event) {
+addPhotoButton.addEventListener("click", function (event) {
   event.preventDefault();
   popupPhoto.classList.toggle("popup_is_opened");
-})
+});
 popupOpen(popupPhoto);
+
+//Функция редактирования имени профиля
+function submitProfileHandler (evt) {
+  evt.preventDefault();
+
+  profileName.textContent = popupProfileName.value;
+  profileJob.textContent = popupProfileJob.value;
+
+  popupProfile.classList.toggle("popup_is_opened");
+};
+
+profileForm.addEventListener("submit", submitProfileHandler);
+
+//Фотокарточки
+
+//Сабмит добавления фотокарточки
+function submitPhotoHandler (evt) {
+  evt.preventDefault();
+
+  renderCards({name: popupPhotoTitle.value, link: popupPhotoLink.value });
+  popupPhotoTitle.value = '';
+  popupPhotoLink.value  = '';
+
+  popupPhoto.classList.toggle("popup_is_opened");
+};
+
+//Лайк фотокарточки
+function likeButton (evt) {
+  evt.target.classList.toggle("card__like-button_active");
+};
+
+//Удаление фотокарточки
+
+//Увеличение фотокарточки 
 
 //Шаблоны
 const cardTemplate = page
@@ -55,20 +89,18 @@ const initialContainer = page.querySelector(".cards");
 //Подгрузка начальных карточек
 function preloadCard(initialItem) {
   const newCard = cardTemplate.cloneNode(true);
-  const titleCard = newCard.querySelector(".card__title");
-  const imageCard = newCard.querySelector(".card__image");
-  //Лайки
-  const likeButton = newCard
-    .querySelector(".card__like-button")
-    .addEventListener("click", function (evt) {
-      evt.target.classList.toggle("card__like-button_active");
-    });
 
+  const titleCard = newCard.querySelector(".card__title");
   titleCard.textContent = initialItem.name;
+
+  const imageCard = newCard.querySelector(".card__image");
   imageCard.setAttribute("src", initialItem.link);
+  //Лайки
+  const likeButtonCheck = newCard.querySelector(".card__like-button");
+  likeButtonCheck.addEventListener("click", likeButton);
 
   return newCard;
-}
+};
 //Рендер карточки
 function renderCards(initialItem) {
   initialContainer.append(preloadCard(initialItem));
@@ -77,3 +109,5 @@ function renderCards(initialItem) {
 initialCards.forEach((initialItem) => {
   renderCards(initialItem);
 });
+
+photoForm.addEventListener('submit', submitPhotoHandler);
