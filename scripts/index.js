68b99fcef5b475121ba538c3d05104f832cffe14
Elizaveta Kruchinina <Kruchinina.elizaveta1@yandex.ru>
2022-06-25 23:48:35 +0300

    добавление функции открытия карточки

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,7 @@ const addPhotoButton = page.querySelector(".profile__add-button");
 //Попапы
 const popupProfile = page.querySelector(".popup_profile");
 const popupPhoto = page.querySelector(".popup_photo");
+const popupPhotoOpen = page.querySelector(".popup_photo_open");
 
 // Инпуты, форма и кнопки попапа для профиля
 const profileForm = popupProfile.querySelector(".form_profile");
@@ -72,19 +73,41 @@ function submitPhotoHandler (evt) {
 };
 
 //Лайк фотокарточки
-function likeButton (evt) {
+function likeButtonHandler (evt) {
   evt.target.classList.toggle("card__like-button_active");
 };
 
 //Удаление фотокарточки
+function deleteCardHandler (evt) {
+  evt.target.closest(".card").remove();
+}
 
-//Увеличение фотокарточки 
+const photoCloseBtn = popupPhotoOpen.querySelector(".popup_type_photo");
+photoCloseBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  popupPhotoOpen.classList.toggle("popup_is_opened");
+});
+
+
+//Открытие фотокарточки
+function openPhotoHandler (e) {
+  const target = e.currentTarget;
+  const cardTitle = target.parentNode.querySelector('.card__title');
+  const cardImage = target.querySelector('.card__image');
+  const fullPhoto = popupPhotoOpen.querySelector(".popup__big");
+  const photoSubtitle = popupPhotoOpen.querySelector(".popup__subtitle");
+
+  fullPhoto.src = cardImage.src;
+  photoSubtitle.textContent = cardTitle.textContent;
+  popupPhotoOpen.classList.toggle("popup_is_opened");
+};
+
 
 //Шаблоны
 const cardTemplate = page
   .querySelector("#card-template")
   .content.querySelector(".card");
-//Контейнер ДОМ
+//Контейнер
 const initialContainer = page.querySelector(".cards");
 //Подгрузка начальных карточек
 function preloadCard(initialItem) {
@@ -95,15 +118,21 @@ function preloadCard(initialItem) {
 
   const imageCard = newCard.querySelector(".card__image");
   imageCard.setAttribute("src", initialItem.link);
-  //Лайки
-  const likeButtonCheck = newCard.querySelector(".card__like-button");
-  likeButtonCheck.addEventListener("click", likeButton);
+  
+  const likeButton = newCard.querySelector(".card__like-button");
+  likeButton.addEventListener("click", likeButtonHandler);
+
+  const deleteCard = newCard.querySelector(".card__delete-button");
+  deleteCard.addEventListener("click", deleteCardHandler);
+
+  const openedPhoto = newCard.querySelector(".card__pointer");
+  openedPhoto.addEventListener("click", (e) => openPhotoHandler(e));
 
   return newCard;
 };
 //Рендер карточки
 function renderCards(initialItem) {
-  initialContainer.append(preloadCard(initialItem));
+  initialContainer.prepend(preloadCard(initialItem));
 }
 // Загрузка карточки
 initialCards.forEach((initialItem) => {
